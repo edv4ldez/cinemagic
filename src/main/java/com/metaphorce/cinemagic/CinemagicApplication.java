c7a3910e77d6677ae6411d6a895beb1687100dc0
Edwin Valdez <ed.9820v@gmail.com>
2024-07-21 19:03:17 -0600

    Starting project

---
+++ src/main/java/com/metaphorce/cinemagic/CinemagicApplication.java
@@ -0,0 +1,11 @@
+package com.metaphorce.cinemagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemagicApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CinemagicApplication.class, args);
+	}
+}
---
+++ src/test/java/com/metaphorce/cinemagic/CinemagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaphorce.cinemagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
